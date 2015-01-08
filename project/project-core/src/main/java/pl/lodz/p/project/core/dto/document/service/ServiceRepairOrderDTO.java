b7d5e04b970df2da9a51fdb467d4035f64440f95
Bartosz Milczarek <bartosz@milczarek.it>
2015-01-08 22:10:23 +0100

    Zgłoszenie serwisowe - UI

--- project/project-core/src/main/java/pl/lodz/p/project/core/dto/document/service/ServiceRepairOrderDTO.java
+++ project/project-core/src/main/java/pl/lodz/p/project/core/dto/document/service/ServiceRepairOrderDTO.java
@@ -5,52 +5,62 @@ import pl.lodz.p.project.core.dto.contractor.ContractorDTO;
 
 public class ServiceRepairOrderDTO extends BaseServiceDocumentDTO {
 
-	public ServiceRepairOrderDTO() {
-		super(ServiceDocumentType.REPAIR_ORDER);
-	}
-	
-	private ContractorDTO contractor;
-	private String equipentInfo;
-	private boolean guarantee;
-	private String guaranteeNo;
-	private String saleDocumentNo;
-	private String description;
+    private ContractorDTO contractor;
+    private String equipentInfo;
+    private boolean guarantee;
+    private String guaranteeNo;
+    private String saleDocumentNo;
+    private String description;
+    public ServiceRepairOrderDTO() {
+        super(ServiceDocumentType.REPAIR_ORDER);
+    }
 
-	public ContractorDTO getContractor() {
-		return contractor;
-	}
-	public void setContractor(ContractorDTO contractor) {
-		this.contractor = contractor;
-	}
-	public String getEquipentInfo() {
-		return equipentInfo;
-	}
-	public void setEquipentInfo(String equipentInfo) {
-		this.equipentInfo = equipentInfo;
-	}
-	public boolean isGuarantee() {
-		return guarantee;
-	}
-	public void setGuarantee(boolean guarantee) {
-		this.guarantee = guarantee;
-	}
-	public String getGuaranteeNo() {
-		return guaranteeNo;
-	}
-	public void setGuaranteeNo(String guaranteeNo) {
-		this.guaranteeNo = guaranteeNo;
-	}
-	public String getSaleDocumentNo() {
-		return saleDocumentNo;
-	}
-	public void setSaleDocumentNo(String saleDocumentNo) {
-		this.saleDocumentNo = saleDocumentNo;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
+    public ContractorDTO getContractor() {
+        return contractor;
+    }
+
+    public void setContractor(ContractorDTO contractor) {
+        this.contractor = contractor;
+    }
+
+    public String getEquipentInfo() {
+        return equipentInfo;
+    }
+
+    public void setEquipentInfo(String equipentInfo) {
+        this.equipentInfo = equipentInfo;
+    }
+
+    public boolean isGuarantee() {
+        return guarantee;
+    }
+
+    public void setGuarantee(boolean guarantee) {
+        this.guarantee = guarantee;
+    }
+
+    public String getGuaranteeNo() {
+        return guaranteeNo;
+    }
+
+    public void setGuaranteeNo(String guaranteeNo) {
+        this.guaranteeNo = guaranteeNo;
+    }
+
+    public String getSaleDocumentNo() {
+        return saleDocumentNo;
+    }
+
+    public void setSaleDocumentNo(String saleDocumentNo) {
+        this.saleDocumentNo = saleDocumentNo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
--- project/project-core/src/main/java/pl/lodz/p/project/core/jsf/documents/service/ServiceRepairOrderTemplateBean.java
+++ project/project-core/src/main/java/pl/lodz/p/project/core/jsf/documents/service/ServiceRepairOrderTemplateBean.java
@@ -1,63 +1,60 @@
 package pl.lodz.p.project.core.jsf.documents.service;
 
-import java.io.Serializable;
-import java.util.Date;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Scope;
+import pl.lodz.p.project.core.domain.document.service.ServiceDocumentState;
+import pl.lodz.p.project.core.dto.document.service.ServiceRepairOrderDTO;
 
 import javax.annotation.PostConstruct;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.context.annotation.Scope;
-
-import pl.lodz.p.project.core.domain.document.service.ServiceDocumentState;
-import pl.lodz.p.project.core.dto.document.service.ServiceRepairOrderDTO;
+import java.io.Serializable;
+import java.util.Date;
 
 @Scope("request")
 @Named("serviceRepairOrderTemplateBean")
 public class ServiceRepairOrderTemplateBean implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-    
+
     private ServiceRepairOrderDTO document;
-    
+
     @PostConstruct
     public void init() {
         String id = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("id");
         if (StringUtils.isBlank(id)) {
-        	document = createNew();
+            document = createNew();
         } else {
-        	// TODO
-        	document = null;
+            // TODO
+            document = null;
         }
-    }    
-    
+    }
+
     private ServiceRepairOrderDTO createNew() {
-    	ServiceRepairOrderDTO document = new ServiceRepairOrderDTO();
-    	document.setState(ServiceDocumentState.NEW);
-    	document.setCreationDate(new Date());
-    	
-    	document.setSymbol("SRO/15/01/2015");
-    	document.setDescription("Tutaj opis usterki");
-    	document.setEquipentInfo("Laptop DELL Inspirion 15");
-    	document.setGuarantee(true);
-    	document.setGuaranteeNo("1254/2014");
-    	document.setSaleDocumentNo("ABC/3213");
-    	
-    	return document;
+        ServiceRepairOrderDTO document = new ServiceRepairOrderDTO();
+        document.setState(ServiceDocumentState.NEW);
+        document.setCreationDate(new Date());
+
+        document.setSymbol("SRO/16/01/2015");
+        document.setDescription("Tutaj opis usterki");
+        document.setEquipentInfo("Laptop DELL Inspirion 15");
+        document.setGuarantee(true);
+        document.setGuaranteeNo("1254/2014");
+        document.setSaleDocumentNo("ABC/3213");
+        return document;
     }
-   
+
     public void clear() {
-    	document = createNew();
+        document = createNew();
     }
 
-	public ServiceRepairOrderDTO getDocument() {
-		return document;
-	}
+    public ServiceRepairOrderDTO getDocument() {
+        return document;
+    }
 
-	public void setDocument(ServiceRepairOrderDTO document) {
-		this.document = document;
-	}
+    public void setDocument(ServiceRepairOrderDTO document) {
+        this.document = document;
+    }
 
 }
