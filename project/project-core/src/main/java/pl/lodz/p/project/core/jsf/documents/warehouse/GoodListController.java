550f932582e8c1caaa05d20272f807fdfe5175dc
Janiu <janiu1990@gmail.com>
2015-01-15 02:44:23 +0100

    Poprawione kilka formatek po ostatnim commicie.

--- project/project-core/src/main/java/pl/lodz/p/project/core/jsf/documents/warehouse/GoodListController.java
+++ project/project-core/src/main/java/pl/lodz/p/project/core/jsf/documents/warehouse/GoodListController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import pl.lodz.p.project.core.dto.good.GoodDTO;
 import pl.lodz.p.project.core.jsf.base.EditListController;
+import pl.lodz.p.project.core.jsf.base.GUI;
 import pl.lodz.p.project.core.service.good.GoodServiceImpl;
 
 import javax.annotation.PostConstruct;
@@ -18,6 +19,9 @@ public class GoodListController extends EditListController<GoodDTO> {
 
     @Autowired
     private GoodServiceImpl goodService;
+
+    @Autowired
+    private GUI gui;
 
     private Double quantity;
 
@@ -34,5 +38,10 @@ public class GoodListController extends EditListController<GoodDTO> {
 
     public void setQuantity(Double quantity) {
         this.quantity = quantity;
+    }
+
+    @Override
+    public String edit(String id) {
+        return gui.redirect("addEditGood", id);
     }
 }
